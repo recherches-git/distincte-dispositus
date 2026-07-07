8f5cc0118312d4ba7600520d67dc6540e85fae1d
recherches-git <recherches.github@hotmail.com>
2026-07-07 14:23:37 -0400

    dev

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ app.get('/index', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/404', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/404.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Locally available on PORT:3000 for dev.`);
 });
